dada1174695a07d0173df36e0f3d7f28fc3f0470
YashLagare <yashlagare77@gmail.com>
2026-01-24 11:12:51 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     env: {
+//         TEMPLATE_ID : process.env.TEMPLATE_ID,
+//         SERVICE_ID : process.env.SERVICE_ID,
+//         PUBLIC_KEY : process.env.PUBLIC_KEY,
+//     },
+// }
+
+// module.exports = nextConfig
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    env: {
-        TEMPLATE_ID : process.env.TEMPLATE_ID,
-        SERVICE_ID : process.env.SERVICE_ID,
-        PUBLIC_KEY : process.env.PUBLIC_KEY,
-    },
-}
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
