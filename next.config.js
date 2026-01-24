2cfc588bac57861b68ed041ca4e18f651d43f9a4
YashLagare <yashlagare77@gmail.com>
2026-01-24 11:01:42 +0530

    Configure Next.js static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     env: {
         TEMPLATE_ID : process.env.TEMPLATE_ID,
         SERVICE_ID : process.env.SERVICE_ID,
