54be4f8f64e73fc1a3bb9e11850a4cdbf0417763
YashLagare <yashlagare77@gmail.com>
2026-09-01 16:28:30 +0530

    earth-animation-issue-solved

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -10,7 +10,7 @@ const Earth = () => {
   const earth = useGLTF(modelPath);
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={1.7} position-y={0} rotation-y={0} />
   );
 };
 
@@ -22,10 +22,10 @@ const EarthCanvas = () => {
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov: 45,
+        fov: 50,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6],
+        position: [-3.5, 2.8, 7],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
